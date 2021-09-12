59d645d3a4744d8904258938f6ac071e0a002556
tripaak <tripaak.akash@gmail.com>
2021-09-12 19:42:22 +0200

    / route

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
 
+@app.route('/')
 @app.route('/index.html')
 def index():
     return render_template("index.html"), 200
